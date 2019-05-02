74bf1f28849f1d8c61bdf3d5fbfa52f0cffa9a33
Marc Schickedanz <marc.schickedanz@pivotfox.de>
2019-05-02 20:33:28 +0200

    added host change

--- scripts/startKivi.sh
+++ scripts/startKivi.sh
@@ -3,6 +3,7 @@
 sed -i "/admin_password/c\admin_password = $ADMIN_PASSWORD" /var/www/kivitendo-erp/config/kivitendo.conf
 sed -i "/user     =/c\user     = $POSTGRES_USER" /var/www/kivitendo-erp/config/kivitendo.conf
 sed -i "/password =/c\password = $POSTGRES_PASSWORD" /var/www/kivitendo-erp/config/kivitendo.conf
+sed -i "/host     =/c\host     = $HOST" /var/www/kivitendo-erp/config/kivitendo.conf
 
 
 #host     = postgres_container
